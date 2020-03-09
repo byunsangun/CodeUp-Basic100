6db0499699abbceec0192b16fadc9c52eec119fc
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-10 03:48:48 +0900

    1082

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-	int a,b;
-	scanf("%d %d", &a, &b);
-	for(int i =1; i<=a; i++){
-		for(int n = 1; n<=b; n++){
-			printf("%d %d\n", i, n);
-		}
+	 int a;
+	scanf("%X", &a);
+	for(int i = 1; i<16; i++){
+		printf("%X*%X=%X\n",a,i, a * i);
 	}
-	
+	return 0;
 }
