39313609c77e8d3bbd2f54a52b491fa22bf0620d
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-10 02:24:27 +0900

    1070

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
 int main (void){
-	char a;
-	scanf("%c", &a);
-	if(a == 'A'){
-		printf("best!!!");
-	}
-	else if(a == 'B'){
-		printf("good!!");
-	}
-	else if(a == 'C'){
-		printf("run!");
-	}
-	else if(a == 'D'){
-		printf("slowly~");
-	}
-	else {
-		printf("what?");
+	int a;
+	scanf("%d", &a);
+	switch(a){
+		case 12:
+		case 1:
+		case 2:
+			printf("winter");
+		break;
+		case 3:
+		case 4:
+		case 5:
+			printf("spring");
+		break;
+		case 6:
+		case 7:
+		case 8:
+			printf("summer");
+		break;
+		case 9:
+		case 10:
+		case 11:
+			printf("fall");
+		break;
+		
 	}
 }
