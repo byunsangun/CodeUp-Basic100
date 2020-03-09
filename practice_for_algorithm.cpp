3342b35b418c78cb9449e30b59155b4dff4c4737
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-10 02:01:03 +0900

    1065

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -3,5 +3,14 @@
 int main (void){
 	int a,b,c;
 	scanf("%d %d %d", &a, &b, &c);
-	printf("%d", (a>b)? ((b>c)? c : b) : ((a>c)? c : a));
+	
+	if(a % 2 == 0){
+		printf("%d\n", a);
+	}
+	if(b % 2 == 0){
+		printf("%d\n", b);
+	}
+	 if(c % 2 == 0){
+		printf("%d\n", c);
+	}
 }
