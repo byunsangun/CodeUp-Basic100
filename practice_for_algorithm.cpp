2cd978540acc89c4ca918e89af36b2cb6e2d2582
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-09 18:49:48 +0900

    1026

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main (void){
-	int a,b,c,d,e;
-	scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
-	printf("\[%d\]\n\[%d\]\n\[%d\]\n\[%d\]\n\[%d\]", a*10000, b*1000, c*100, d*10, e*1);
-	return 0;
+	int hour, minute, second;
+	scanf("%d:%d:%d", &hour, &minute, &second);
+	printf("%d", minute);
 }
-
