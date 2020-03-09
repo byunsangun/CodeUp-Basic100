f0737c5b147339b2d176ef79fd6dc999d1eac99f
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-09 19:32:19 +0900

    1047

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main (void){
-	long long int a,b,c;
-	scanf("%lld %lld %lld", &a, &b, &c);
-	printf("%lld\n", a+b+c);
-	printf("%.1f", (float)(a+b+c)/3);
-	return 0;
+	int a;
+	scanf("%d", &a);
+	printf("%d", a<<1);
 }
