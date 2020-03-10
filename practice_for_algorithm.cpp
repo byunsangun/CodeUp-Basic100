ffd7fe6c970089d626b2c3ad55bcd65049abb7fa
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-11 01:30:01 +0900

    1097

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -3,18 +3,41 @@
 
 int main(void){
 	int a[20][20]={};
-	int x,y;
 	int n;
-	scanf("%d", &n);
-	for(int i = 0; i<n; i++){
+	
+	int x,y;
+	for(int i=1;i<=19;i++){
+	   for(int j=1;j<=19;j++){
+  		scanf("%d", &a[i][j]);
+   }
+ }
+ 	scanf("%d", &n);
+ 	
+	for(int i = 0; i<n; i++){	
 		scanf("%d %d", &x, &y);
-		a[x][y] = 1;
-	}
-	for(int z = 1; z<=19; z++){
+	
 		for(int j = 1; j<=19; j++){
-			printf("%d ", a[z][j]);
+			if(a[x][j] == 0){
+				a[x][j] = 1;
+			}
+			else{
+				a[x][j] = 0;
+			}
+		}
+		for(int j = 1; j<=19; j++){
+			if(a[j][y] == 0){
+				a[j][y] = 1;
+			}
+			else{
+				a[j][y] = 0;
+			}
+		}
+}
+	for(int i = 1; i<=19; i++){
+		for(int j = 1; j<=19; j++){
+			printf("%d ", a[i][j]);
 		}
 		printf("\n");
 	}
-}
+	}
 
