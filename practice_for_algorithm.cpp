63686964ca0bc6b50d30e740eb67039b7e81a589
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-10 11:55:22 +0900

    1085

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-	 int a,b,c;
-	scanf("%d %d %d", &a, &b, &c);
-	for(int i = 0; i<a; i++){
-		for(int l = 0; l <b; l++){
-			for(int n = 0; n<c; n++){
-				printf("%d %d %d\n", i, l, n);
-			}
-		}
-}
-printf("%d", a*b*c);
-	return 0;
+	int a,b,c,d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	printf("%.1f MB", (float)a*b*c*d/8/1024/1024);
+	
 }
