bee72586e8c9b6403817f179dfbb65191628477f
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-10 21:56:51 +0900

    1091

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -2,8 +2,10 @@
 #include <math.h>
 
 int main(void){
-	long long a,b,c;
-	scanf("%lld %lld %lld", &a, &b, &c);
-	a = a * pow(b,c-1);
+	long long a,b,c,d;
+	scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+	for(int i = 0; i<d-1; i++){
+		a = a * b + c;
+	}
 	printf("%lld", a);
 }
