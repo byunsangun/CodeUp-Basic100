c588b68611eecb0127563ef29f68d353e1d55992
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-03-10 11:20:32 +0900

    1083

--- practice_for_algorithm.cpp
+++ practice_for_algorithm.cpp
@@ -3,8 +3,14 @@
 int main(void){
 	 int a;
 	scanf("%X", &a);
-	for(int i = 1; i<16; i++){
-		printf("%X*%X=%X\n",a,i, a * i);
+	for(int i = 1; i<=a; i++){
+		if(i % 3 == 0){
+			printf("X ");
+		}
+		else{
+		
+		printf("%d ",i);
 	}
+}
 	return 0;
 }
